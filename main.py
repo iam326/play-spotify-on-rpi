6a268ecdbed30e83d4b042bf6b6471fe7842749b
t.takahashi <takahashi.developer@gmail.com>
2020-05-20 00:06:56 +0900

    add comment

--- main.py
+++ main.py
@@ -13,22 +13,35 @@ export SPOTIPY_CLIENT_SECRET='zzz'
 export SPOTIPY_REDIRECT_URI='http://...'
 """
 
-NEXT=23
-START_STOP=24
-PREV=25
-PLAY_STATE=False
+NEXT = 23
+START_STOP = 24
+PREV = 25
+PLAY_STATE = False
 
 username = os.environ['SPOTIPY_USERNAME']
 scope = 'user-read-playback-state,user-modify-playback-state'
 
 try:
+  # http://sakanaaas.hateblo.jp/entry/2017/07/02/122740
+  # 本来であればここでブラウザが起動する
+  # 認証確認をOKした後に遷移するページのURLをターミナルに貼り付けることで
+  # tokenを取得することができる
+
+  # が、SSHでRaspberry Piに接続している場合、ブラウザを起動できず、ここで処理が止まる
+  # 一時的な対応方法として、Mac側にて同じ認証方法でtokenを取得すると生成されるファイル
+  # .cache-{username} を、このファイルが存在するディレクトリにコピーすると、
+  # tokenの有効期限が切れるまでは、ここの処理を通すことができる
   token = util.prompt_for_user_token(username, scope)
 except (AttributeError, JSONDecodeError):
-  os.remove(".cache-"+username)
+  # https://github.com/plamere/spotipy/issues/176#issuecomment-307632735
+  # 変なキャッシュが残ることが原因で?認証に失敗するバグへの対応
+  os.remove(".cache-" + username)
   token = util.prompt_for_user_token(username, scope)
 
 sp = spotipy.Spotify(auth=token)
 
+# https://github.com/plamere/spotipy/issues/211#issuecomment-369863112
+# 古いVersionのSpotipyだとdevices()がundefinedになるため、上記の方法でUpgradeする
 devices = sp.devices()
 target_device_id = None
 
