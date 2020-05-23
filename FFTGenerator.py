8817e7cb728ffb65499c7e5daba3e985cddeee4a
t.takahashi <takahashi.developer@gmail.com>
2020-05-23 22:59:07 +0900

    add FFTGenerator

---
+++ FFTGenerator.py
@@ -0,0 +1,44 @@
+import pyaudio
+import numpy as np
+
+RATE = 22000 # 22kHz
+CHUNK = 1024
+
+class FFTGenerator():
+  def __init__(self):
+    self.audio = pyaudio.PyAudio()
+    self.stream = self.audio.open(format=pyaudio.paInt16,
+                             channels=1,
+                             rate=RATE,
+                             input=True,
+                             output=False,
+                             frames_per_buffer=CHUNK)
+
+  def __del__(self):
+    self.stream.stop_stream()
+    self.stream.close()
+    self.audio.terminate()
+
+  def is_active(self):
+    return self.stream.is_active()
+
+  def read(self):
+    input = self.stream.read(CHUNK, exception_on_overflow=False)
+    ndarray = np.frombuffer(input, dtype='int16') / 32768
+    fft = np.fft.fft(ndarray)
+    fft = np.abs(fft) ** 2
+    fft = fft[0:int(CHUNK / 2)]
+    return 20 * np.log10(fft + 0.0001)
+
+if __name__ == '__main__':
+  fft = FFTGenerator()
+
+  try:
+    while True:
+      if fft.is_active():
+        print(fft.read())
+  except KeyboardInterrupt:
+    pass
+
+  del fft
+
